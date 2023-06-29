bdaac422656678314c987b6ed399bd9ebc817f14
QUOTIENT <tjsgh5965@gmail.com>
2023-06-29 14:51:24 +0900

    MBTI BUTTON

--- myword/src/main/java/com/example/myword/MainActivity.kt
+++ myword/src/main/java/com/example/myword/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.myword
 
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import androidx.activity.ComponentActivity
 
@@ -11,21 +12,31 @@ class MainActivity : ComponentActivity() {
 
         val textView = findViewById<TextView>(R.id.textView)
         textView.text = getWord()
+
+        val button = findViewById<Button>(R.id.button3)
+        button.setOnClickListener({
+            textView.text = getWord()
+        })
     }
 
     private fun getWord(): String {
         val list = listOf(
-            "나까지 나설 필요는 없다",
-            "헌신하면 헌신짝 된다",
-            "참고 참고 또 참으면 참나무가 된다",
-            "포기하면 편하다",
-            "왕관을 쓰려는 자, 그 무게를 견뎌라",
-            "아니면 말고",
-            "나도 나지만 너도 너다",
-            "목숨을 버리면 무기만은 살려 주겠다",
-            "가는 말이 고우면 사람을 얕본다",
-            "사람은 원래 똥을 싸는 법이다",
-            "대마왕 뿔은 10g에 10만원이다",
+            "ISTJ",
+            "ISTP",
+            "INFJ",
+            "INTJ",
+            "ISFJ",
+            "ISFP",
+            "INFP",
+            "INTP",
+            "ESTJ",
+            "ESFP",
+            "ENFP",
+            "ENTP",
+            "ESFJ",
+            "ESTP",
+            "ENFJ",
+            "ENTJ"
         )
 
         return list.random()
